94766969357b724f40ecc89a8ca05af602f84cdb
mfranklin@mitre.org <mfranklin@mitre.org@1ae130c9-2b25-0426-bda3-53af0882bf06>
2011-04-12 18:20:24 +0000

    Added skeleton tests for other JPA entities
    git-svn-id: https://pushee.googlecode.com/svn/trunk/pushee@20 1ae130c9-2b25-0426-bda3-53af0882bf06

--- src/test/java/org/mitre/pushee/hub/repository/PublisherRepositoryTest.java
+++ src/test/java/org/mitre/pushee/hub/repository/PublisherRepositoryTest.java
@@ -1,9 +1,15 @@
 package org.mitre.pushee.hub.repository;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
 /**
  * @author mfranklin
  *         Date: 4/12/11
  *         Time: 11:08 AM
  */
 public class PublisherRepositoryTest {
+    @Test
+    @Ignore
+    public void foo() {}
 }
--- src/test/java/org/mitre/pushee/hub/repository/SubscriberRepositoryTest.java
+++ src/test/java/org/mitre/pushee/hub/repository/SubscriberRepositoryTest.java
@@ -1,9 +1,15 @@
 package org.mitre.pushee.hub.repository;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
 /**
  * @author mfranklin
  *         Date: 4/12/11
  *         Time: 11:09 AM
  */
 public class SubscriberRepositoryTest {
+    @Test
+    @Ignore
+    public void foo() {}
 }
