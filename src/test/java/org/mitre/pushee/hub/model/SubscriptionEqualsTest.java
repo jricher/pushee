3c11bf26ff2258995db386fde1c33b96966fb736
aanganes@mitre.org <aanganes@mitre.org@1ae130c9-2b25-0426-bda3-53af0882bf06>
2011-04-20 00:38:23 +0000

    Added SubscriptionEqualsTest to test overridden .equals method for Subscription objects.
    git-svn-id: https://pushee.googlecode.com/svn/trunk/pushee@30 1ae130c9-2b25-0426-bda3-53af0882bf06

---
+++ src/test/java/org/mitre/pushee/hub/model/SubscriptionEqualsTest.java
@@ -0,0 +1,87 @@
+package org.mitre.pushee.hub.model;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+/**
+ * Test for the Subscription .equals method
+ * 
+ * @author AANGANES
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/application-context.xml", "classpath:test-context.xml"})
+public class SubscriptionEqualsTest extends TestCase {
+
+	//SubA and subB should be equal.
+	private Subscription subA;
+	private Subscription subB;
+	//SubC should not be equal to A or B
+	private Subscription subC;
+	
+	private Feed theFeed;
+	private Feed theOtherFeed;
+	private Subscriber theSubscriber;
+	
+	@Before
+	public void setup()
+	{
+		//Set up Feeds and the subscriber
+		theFeed = new Feed();
+		theFeed.setId(1L);
+		theFeed.setUrl("http://example.com/feed");
+		
+		theOtherFeed = new Feed();
+		theOtherFeed.setId(2L);
+		theOtherFeed.setUrl("http://example.com/theOtherFeed");
+		
+		theSubscriber = new Subscriber();
+		theSubscriber.setId(1L);
+		theSubscriber.setPostbackURL("http://example.com/subscriber");
+		
+		subA = new Subscription();
+		subA.setFeed(theFeed);
+		subA.setSubscriber(theSubscriber);
+		
+		subB = new Subscription();
+		subB.setFeed(theFeed);
+		subB.setSubscriber(theSubscriber);
+		
+		subC = new Subscription();
+		subC.setFeed(theOtherFeed);
+		subC.setSubscriber(theSubscriber);
+				
+	}
+	
+	@Test
+	public void testReflexive() 
+	{
+		assertEquals(subA, subA);
+		assertEquals(subB, subB);
+		assertEquals(subC, subC);
+	}
+	
+	@Test
+	public void testSymmetric() 
+	{
+		assertEquals(subA, subB);
+		assertEquals(subB, subA);
+		
+		assertFalse(subA.equals(subC));
+		assertFalse(subC.equals(subA));
+	}
+	
+	@Test
+	public void testNotEqualToNull() 
+	{
+		assertFalse(subA.equals(null));
+		assertFalse(subC.equals(null));
+	}
+	
+}
